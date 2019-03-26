c94fc339350434c6d3d7fd079ddfa7622021b1b2
AnjuKukreja <anju.k.kukreja@gmail.com>
2019-03-26 14:01:40 +0530

    incorporating review comments to change logic to verify whether unzip is working properly or not

--- test/inputoutput/test_io.py
+++ test/inputoutput/test_io.py
@@ -1,13 +1,23 @@
-from main.inputoutput.io import inc,unzip_file
+from main.inputoutput.io import inc, unzip_file
 import os
+
+
+def diff(li1, li2):
+    return list(set(li1) - set(li2))
+
 
 def test_answer():
     assert inc(3) == 5
 
+
 def test_unzip_file():
-    fromPath=r"test\data\Desktop.zip"
-    toPath=r"test\data\target"
-    curDir=os.getcwd()
-    fromPath=os.path.join(str(curDir),str(fromPath))
-    toPath=os.path.join(str(curDir),str(toPath))
-    assert unzip_file(fromPath,toPath) == 1
+    from_path = os.path.join("..", "data", "TestFile.zip")
+    to_path = os.path.join("..", "data", "target")
+    unzip_file(from_path, to_path)
+    extracted_files = os.listdir(to_path)
+    file_list = set(file for file in os.listdir(os.path.join("..", "data"))
+                      if os.path.isfile(os.path.join("..", "data", file)))
+    file_list.discard("TestFile.zip")
+    diff_files = file_list - set(extracted_files)
+    assert diff_files.__len__() == 0
+
